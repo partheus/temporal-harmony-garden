39b934d11dbbfe7f65fd1836df282edacc9db131
Parth <parth.shiralkar@gmail.com>
2023-10-02 14:00:22 -0700

    Create basic blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Parth Shiralkar",
+	url: "https://parth.ninja/",
+	language: "en",
+	description: "My experiences as a time traveler.",
+	author: {
+		name: "Parth Shiralkar",
+		email: "parth.shiralkar@gmail.com",
+		url: "https://parth.ninja/about/"
+	}
+}
